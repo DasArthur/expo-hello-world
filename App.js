f202248f1d6dbe02e6baba5d4c42d4cb3090a37c
Arturs Petrovskis <a.petrovskis3@gmail.com>
2020-12-29 08:07:19 +0200

    Hello Elon tutorial done

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <ImageBackground
+        style={{
+          flex: 1,
+          width: "100%",
+          height: "100%"
+        }}
+        resizeMode={"cover"}
+        source={{uri: "https://api.time.com/wp-content/uploads/2015/05/elon-musk.jpg"}}
+      >
+
+      <View style={styles.container}>
+        <Text style={{
+          fontSize: 110,
+          fontWeight: "bold",
+          color: "white",
+          marginBottom: 20,
+          lineHeight: 110,
+        }}>Hello,{"\n"}Elon!</Text>
+
+        <View
+          style={{
+            width: "100%",
+            height: 15,
+            backgroundColor: "white",
+            marginBottom: "40%"
+          }}
+        ></View>
+      </View>
+
+      </ImageBackground>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 20,
+    justifyContent: "flex-end",
   },
 });
